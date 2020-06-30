cf2ba565762bae3ef05583c00ff87aa9ae34a002
Marco <marco.longo@hotmail.it>
2020-06-30 16:12:29 +0200

    Esercizio Campo Minato bozza.

--- js/script.js
+++ js/script.js
@@ -10,18 +10,42 @@
 
 // Dichiarazione variabili
 var arrayMine = [];
+var arrayUser = [];
+var numUser = 0;
 var numRnd = 0;
 var count = 0;
+var score = 0;
+
 
 // Il computer deve generare 16 numeri casuali tra 1 e 100.
 do {
   numRnd = getRandomIntInclusive(1, 100);
-  if(isAbsent(arrayMine, numRnd)) {
+  if(isAbsent(arrayMine, numRnd)) { // I numeri non possono essere duplicati
   arrayMine.push(numRnd);
   }
 } while(arrayMine.length < 16);
 
 console.log(arrayMine);
+
+alert("Benvenuto a Campo Minato! Ho appena piazzato 16 mine su 16 numeri random!");
+
+// In seguito deve chiedere all’utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
+while(arrayUser.length < 84 && isAbsent(arrayMine, numUser)) { // Se il numero è presente nella lista dei numeri generati, la partita termina
+  numUser = parseInt(prompt("Inserisci un numero da 1 a 100: "));
+  if(isAbsent(arrayUser, numUser) && isAbsent(arrayMine, numUser)) {
+    arrayUser.push(numUser);
+    score++;
+    alert("Che fortuna! Il numero " + numUser + " non era una mina. Punteggio: " + score); // , altrimenti si continua chiedendo all’utente un altro numero.
+  } else if(isAbsent(arrayMine, numUser)){ // L’utente non può inserire più volte lo stesso numero.
+    alert("Hai già inserito questo numero. Riprova!")
+  }
+}
+// La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
+if(score < 84) {
+  alert("Boom! Purtroppo il numero " + numUser + " era una mina! Punteggio: " + score); // Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
+} else if(score == 84) {
+  alert("Fantastico! Ti consiglio di andare a Las Vegas, hai evitato tutte le mine. Punteggio: 84");
+}
 
 // Definizione funzioni
 
@@ -38,4 +62,13 @@ function isAbsent(array, number) {
     }
   }
   return true;
+}
+// Definisco funzione speculare per motivi di sanità mentale...
+function isPresent(array, number) {
+  for (var i = 0; i < array.length; i++) {
+    if(array[i] == number) {
+      return true;
+    }
+  }
+  return false;
 }
