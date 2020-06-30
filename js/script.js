03a93fd9aedffaf796f06a716f17420f62787976
Marco <marco.longo@hotmail.it>
2020-06-30 16:48:55 +0200

    Esercizio Campo Minato completo.

--- js/script.js
+++ js/script.js
@@ -9,6 +9,7 @@
 // Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
 
 // Dichiarazione variabili
+
 var arrayMine = [];
 var arrayUser = [];
 var numUser = 0;
@@ -17,42 +18,44 @@ var count = 0;
 var score = 0;
 
 
-// Il computer deve generare 16 numeri casuali tra 1 e 100.
+// Riempio un array con 16 numeri, da 1 a 100, casuali e diversi fra loro sfruttando isAbsent, da me definita in basso
 do {
   numRnd = getRandomIntInclusive(1, 100);
-  if(isAbsent(arrayMine, numRnd)) { // I numeri non possono essere duplicati
+  if(isAbsent(arrayMine, numRnd)) {
   arrayMine.push(numRnd);
   }
 } while(arrayMine.length < 16);
 
-console.log(arrayMine);
+console.log(arrayMine); // Stampo in console l'array con le mine, sperando che l'utente non sia uno sviluppatore web...
 
-alert("Benvenuto a Campo Minato! Ho appena piazzato 16 mine su 16 numeri random!");
+alert("Benvenuto a Campo Minato! Ho appena piazzato 16 mine su 16 numeri random (Da 1 a 100)!");
 
-// In seguito deve chiedere all’utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
-while(arrayUser.length < 84 && isAbsent(arrayMine, numUser)) { // Se il numero è presente nella lista dei numeri generati, la partita termina
+// Definisco un array con i numeri inseriti dall'utente, se il numero non è nell'array con i numeri 'minati', prendo in input il numero.
+while(arrayUser.length < 84 && isAbsent(arrayMine, numUser)) { // Anche l'improbabile caso di vittoria arresterebbe il while
   numUser = parseInt(prompt("Inserisci un numero da 1 a 100: "));
-  if(isAbsent(arrayUser, numUser) && isAbsent(arrayMine, numUser)) {
+  if(numUser < 1 || numUser > 100) { // Controllo di fair play!
+    alert("Cosa non ti è chiaro di \'numero DA 1 A 100\'?");
+  } else if(isAbsent(arrayUser, numUser) && isAbsent(arrayMine, numUser)) { // Condizione (la più stringente) di vittoria
     arrayUser.push(numUser);
     score++;
-    alert("Che fortuna! Il numero " + numUser + " non era una mina. Punteggio: " + score); // , altrimenti si continua chiedendo all’utente un altro numero.
-  } else if(isAbsent(arrayMine, numUser)){ // L’utente non può inserire più volte lo stesso numero.
+    alert("Che fortuna! Il numero " + numUser + " non era una mina. Punteggio: " + score); // Stampo messaggio di vittoria e riavvio una nuova iterazione
+  } else if(isPresent(arrayUser, numUser)){ // Se il numero è già stato inserito dall'utente, mostro un alert e avvio nuova iterazione col prompt
     alert("Hai già inserito questo numero. Riprova!")
   }
 }
-// La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
+// I 2 possibili scenari in cui ci troviamo, dopo il blocco di codice 32-42, sono: ho fatto 84 punti e quindi ho vinto oppure ho beccato una bomba ed interrompo il ciclo (seconda cond del while falsa)
 if(score < 84) {
   alert("Boom! Purtroppo il numero " + numUser + " era una mina! Punteggio: " + score); // Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
 } else if(score == 84) {
   alert("Fantastico! Ti consiglio di andare a Las Vegas, hai evitato tutte le mine. Punteggio: 84");
-}
+  }
 
 // Definizione funzioni
 
 function getRandomIntInclusive(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min; //Il max è incluso e il min è incluso
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 // Definisco una funzione che restituisce true se un numero è presente nell'array e false viceversa.
 function isAbsent(array, number) {
@@ -63,7 +66,7 @@ function isAbsent(array, number) {
   }
   return true;
 }
-// Definisco funzione speculare per motivi di sanità mentale...
+// Definisco funzione speculare per motivi di praticità.
 function isPresent(array, number) {
   for (var i = 0; i < array.length; i++) {
     if(array[i] == number) {
